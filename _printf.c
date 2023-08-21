162092f47421c2dfab23ad5dfdf8d7ce7f0f2882
azziimm7 <Azza>
2023-08-21 03:17:44 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -13,6 +13,11 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
