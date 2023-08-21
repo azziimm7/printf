1746ab7bbb6d8bb96684c55f5de8786a75257123
azziimm7 <Azza>
2023-08-21 09:48:02 -0700

    number

---
+++ number.c
@@ -0,0 +1,61 @@
+#include "main.h"
+ /**
+  * conver - convert function , a clone of itoa
+  * @num: number
+  * @base: base
+  * @flag: argument flags
+  * @params: parameter structure
+  *
+  * Return: parametere string
+  */
+char *convert(long int num, int base, int flag, params_t *params)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
+	(void)params;
+
+	if (1(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+
+	}
+	array = flag & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do {
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+
+}
+/**
+ * print_unsigned - prints unsigned integer numers
+ *@ap: argument pointer
+ *@param: the parametere struct
+ *
+ * Return: bytes printed
+ */
+int print_address(va_list ap, params_t *params)
+{
+	unsigned long int n = va_arg(ap, unsigned long int);
+	char *str;
+
+	if (!n)
+		return (_puts("(nill)"));
+	str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	*--str = 'x';
+	*--str = '0';
+	return (print_number(str,params));
+}
+	
+
+
