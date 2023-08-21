84b6a6c9e5418ac72a246f1ed1454a931952061b
azziimm7 <Azza>
2023-08-21 11:35:17 -0700

    main.h

--- main.h
+++ main.h
@@ -10,6 +10,25 @@
 #define OUTPUT_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CONVERT_LOWERCASE  1
+#define CONVERT_UNSIGNED   2
+
+/**
+ * struct specifier - Struct token
+ *
+ * @specifier: format token
+ * @f: The function associated
+ */
+typedef struct specifier
+{
+	char *specifier;
+	int (*f)(va_list, params_t *);
+} specifier_t;
+
 /* print functions.c module */
 int print_char(va_list ap, params_t *params);
 int print_string(va_list ap, params_t *params);
@@ -26,5 +45,11 @@ int _putchar(int c);
 char *convert(long int num, int base, int flag, params_t *params);
 int print_unsigned(va_list ap, params_t *params);
 int print_address(va_list ap, params_t *params);
+
+/* convert number module */
+print_hex(va_list ap, params_t *params);
+print_HEX(va_list ap, params_t *params);
+print_binary(va_list ap, params_t *params);
+print_octal(va_list ap, params_t *params);
 
 #endif
